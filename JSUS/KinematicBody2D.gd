6261e655ee34f4f9f74ade2ded078967a19fb0fb
Unknown <till@favierweb.de>
2018-08-11 15:20:56 +0200

    refined Walljump
    Stable in what it is doing.
    can only jump to the right

--- JSUS/KinematicBody2D.gd
+++ JSUS/KinematicBody2D.gd
@@ -6,7 +6,6 @@ const JUMP = -500
 export(int) var GRAVITY = 20
 
 var motion = Vector2(0,0)
-var wallFlag = 0
 
 
 
@@ -24,43 +23,24 @@ func _physics_process(delta):
 		motion.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
-	else:
+	elif is_on_floor():
 		motion.x = 0
 		
 	#Jump
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP
-		#set flag for walljump to false
-		wallFlag = 0
 		
-	#Set Flag if character is on wall
+	#if character is on wall
 	if is_on_wall() && !is_on_floor():
-		if motion.x < 0:
-			wallFlag = 1
-		if motion.x > 0:
-			wallFlag = 2
+		#slow down gravity 
+		if motion.y >0:
+			motion.y -= (GRAVITY/4) *2
+		#make walljump by pressing up
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = 1.5*JUMP
+			motion.x = SPEED
 		
-			
-	
-		
-
-	
-	if wallFlag > 0:
-		#TODO find out wich wall we are at
-		#reduce Gravity while on wall
-		if motion.y > 0:
-			motion.y -= GRAVITY/2
-		if wallFlag == 1:
-			if Input.is_action_just_pressed("ui_right"):
-				motion.y = 1.5*JUMP
-				motion.x = SPEED
-				wallFlag = 0
-		if wallFlag == 2:
-			if Input.is_action_just_pressed("ui_left"):
-				motion.y = 1.5*JUMP
-				motion.x = -SPEED
-				wallFlag = 0
-		
-		
-	motion = move_and_slide(motion,UP)
+	var motiontmp = move_and_slide(motion,UP)
+	if is_on_floor():
+		motion = motiontmp
