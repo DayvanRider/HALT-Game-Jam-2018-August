859addc24586091aa03e0558ea0fcaef0375a3de
leogeier <leonard.geier@hotmail.com>
2018-08-13 08:46:47 +0200

    WAV to OGG (smaller and can be looped)

--- JSUS/BackgroundMusic.gd
+++ JSUS/BackgroundMusic.gd
@@ -3,16 +3,12 @@ extends Node
 var player = null
 
 func _ready():
-	print("Background music")
-	var stream = load("res://assets/Audio/Music/egypt.wav")
+	var stream = load("res://assets/Audio/Music/egypt.ogg")
+	stream.set_loop(true)
+	
 	player = AudioStreamPlayer.new()
 	add_child(player)
 	player.set_stream(stream)
 	player.set_volume_db(-12)
 	player.play()
 	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
