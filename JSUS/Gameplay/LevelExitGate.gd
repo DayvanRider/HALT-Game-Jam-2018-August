b030d727dd55ca78ef68426a1ed76080bc422bb5
Unknown <hecobroe@gmx.de>
2018-08-13 13:02:47 +0200

    Added Final Gate

---
+++ JSUS/Gameplay/LevelExitGate.gd
@@ -0,0 +1,50 @@
+extends Node
+
+export(int) var GemsNeeded
+var gemsCollected
+var gemsTotal
+var scoreText
+func _ready():
+	
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _process(delta):
+	gemsTotal = 0
+	gemsCollected = 0
+
+	var bodies = get_node("Area2D").get_overlapping_bodies()
+	var gems = get_children()
+	var allGemsCollected = true
+
+	for gem in gems:
+		if gem.name.begins_with("Gem"):
+			gemsTotal += 1
+			
+			
+			if gem.get_node("Area2D").is_collected() == true:
+				gemsCollected += 1
+			else:
+				allGemsCollected = false
+				
+	
+	for b in bodies:
+		if b.get_name() == "Player":
+			if allGemsCollected:
+				print("Finish Level")
+				b.kill()
+	
+	scoreText = String(gemsCollected) + " / " + String(gemsTotal)
+	get_node("Score").set_text(scoreText)
+	
+	
+	if allGemsCollected:
+		pass
+		#print("Hooray You finished the Level!")
+		#ADD next Level Command here!
+
+
+
+
+			
