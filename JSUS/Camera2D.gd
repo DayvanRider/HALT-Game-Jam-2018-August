d1b4ced9a3c4a2f675b14ca0e7c037ee4b79b5d9
Unknown <till@favierweb.de>
2018-08-13 14:12:13 +0200

    add screenshake
    still bugged

---
+++ JSUS/Camera2D.gd
@@ -0,0 +1,37 @@
+extends Camera2D
+
+const MAGNITUDE = 50
+const SHAKETIME = 0.05
+var currentpos = get_camera_position()
+
+func _ready():
+	# Initialization here
+	pass
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_page_up"):
+		shake()
+	
+
+func shake():
+	var time = 0
+	while time < SHAKETIME:
+		var pos = Vector2()
+		pos.x = rand_range(-MAGNITUDE,MAGNITUDE)
+		pos.y = rand_range(-MAGNITUDE,MAGNITUDE)
+		self.position = pos
+		
+		time += get_process_delta_time()
+		
+		yield(get_tree(),"idle_frame")
+		
+		self.position = currentpos
+	self.position = Vector2(0,0)
+		
+		
+		
+	
+		
+		
+	
+	
